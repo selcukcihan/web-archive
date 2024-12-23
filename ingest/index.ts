09acc42e1755edf5bcf01522a118f308e9db2db3
Selcuk Cihan <selcukcihan@gmail.com>
2024-12-23 18:22:03 +0300

    Adding script

--- ingest/index.ts
+++ ingest/index.ts
@@ -2,19 +2,10 @@ import { extractTags, summarize } from "./ai";
 import { extractHtmlMetaData } from "./html";
 import { addLink, getUnprocessedLinks, updateLink, storeImageOnS3 } from "./db";
 
-const links = [
-  "https://www.allthingsdistributed.com/2024/12/tech-predictions-for-2025-and-beyond.html",
-  "https://netflixtechblog.com/netflixs-distributed-counter-abstraction-8d0c45eb66b2",
-  "https://aws.amazon.com/blogs/aws/top-announcements-of-aws-reinvent-2024/"
-]
-
+const link = process.argv[3];
+console.log(`Processing ${link}`);
 const runLoop = async () => {
-  for (const link of links) {
-    console.log(`Adding ${link}`);
-    await addLink(link);
-
-  }
-
+  await addLink(link);
   const unprocessed = await getUnprocessedLinks();
   console.log(JSON.stringify(unprocessed, null, 2));
   for (const link of unprocessed) {
